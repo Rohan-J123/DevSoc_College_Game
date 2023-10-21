90a0cfd6e9150f74ecb7474f7a1bba1f36668a73
Rohan-J123 <rohanjha@aol.in>
2023-10-21 14:56:16 +0530

    Added CrossHair

--- DevSoc_College_Game/Assets/Scripts/PlayerAim.cs
+++ DevSoc_College_Game/Assets/Scripts/PlayerAim.cs
@@ -1,20 +1,29 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+using UnityEngine.InputSystem;
 
 public class PlayerAim : MonoBehaviour
 {
-    private PlayerAttack playerAttack;
+    [SerializeField] private PlayerAttack playerAttack;
+    [SerializeField] private LayerMask aimColliderLayerMask = new LayerMask();
+    [SerializeField] private GameObject laserPoint;
+
     // Start is called before the first frame update
     void Start()
     {
         playerAttack = GetComponent<PlayerAttack>();
+        laserPoint = GameObject.FindGameObjectWithTag("LaserPoint");
+
     }
 
     // Update is called once per frame
-    void Update()
+    void FixedUpdate()
     {
-        
+        Vector2 screenCenterPoint = new Vector2(Screen.width / 2f, Screen.height / 2f);
+        Ray ray = Camera.main.ScreenPointToRay(screenCenterPoint);
+        if(Physics.Raycast(ray, out RaycastHit rayCastHit, 1000f, aimColliderLayerMask)){
+            laserPoint.transform.position = rayCastHit.point;
+        }
     }
 }
