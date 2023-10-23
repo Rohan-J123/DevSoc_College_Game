ef1093c202ee362a2736246de35f8af161c470b4
Rohan-J123 <rohanjha@aol.in>
2023-10-23 20:13:28 +0530

    Corrected Player Looting Script

--- DevSoc_College_Game/Assets/Scripts/DeadNPCScript.cs
+++ DevSoc_College_Game/Assets/Scripts/DeadNPCScript.cs
@@ -13,6 +13,9 @@ public class DeadNPCScript : MonoBehaviour
     [SerializeField] private int numberOfBullets;
     [SerializeField] private GameObject player;
     [SerializeField] private TMP_Text collectText;
+    [SerializeField] private GameObject[] deadNPCs;
+    [SerializeField] private bool collected;
+    public bool isNear;
 
     // Start is called before the first frame update
     void Start()
@@ -22,33 +25,49 @@ public class DeadNPCScript : MonoBehaviour
        player = GameObject.FindGameObjectWithTag("Player");
        collectText = GameObject.FindGameObjectWithTag("CollectText").GetComponent<TMP_Text>();
        starterAssetsInputs = player.GetComponent<StarterAssetsInputs>();
+       isNear = false;
+       collected = false;
+       deadNPCs = GameObject.FindGameObjectsWithTag("DeadNPC");
        collectText.SetText("");
     }
 
     // Update is called once per frame
     void Update()
     {
-        if((player.transform.position - transform.position).magnitude < 2.5f){
-            if(weaponBulletType == 1){
-                collectText.SetText(numberOfBullets + " PISTOL BULLETS [PRESS C]");
-                if(starterAssetsInputs.collect){
-                    player.GetComponent<PlayerAttack>().bulletCountPistol += numberOfBullets;
+        if(!collected){
+            if((player.transform.position - transform.position).magnitude < 2.5f){
+                isNear = true;
+                if(weaponBulletType == 1){
+                    collectText.SetText(numberOfBullets + " PISTOL BULLETS [PRESS C]");
+                    if(starterAssetsInputs.collect){
+                        player.GetComponent<PlayerAttack>().bulletCountPistol += numberOfBullets;
+                        collectText.SetText("");
+                        collected = true;
+                    }
+                }
+                else if(weaponBulletType == 2){
+                    collectText.SetText(numberOfBullets * 2 + " ASSAULT BULLETS [PRESS C]");
+                    if(starterAssetsInputs.collect){
+                        player.GetComponent<PlayerAttack>().bulletCountAssault += (numberOfBullets * 2);
+                        collectText.SetText("");
+                        collected = true;
+                    }
+                }
+                
+            }
+            else{
+                isNear = false;
+                bool clear = true;
+                foreach(GameObject deadNPC in deadNPCs)
+                {
+                    if(deadNPC.GetComponent<DeadNPCScript>().isNear == true){
+                        clear = false;
+                    }
+                }
+                if(clear){
                     collectText.SetText("");
-                    Destroy(this);
                 }
             }
-            else if(weaponBulletType == 2){
-                collectText.SetText(numberOfBullets * 2 + " ASSAULT BULLETS [PRESS C]");
-                if(starterAssetsInputs.collect){
-                    player.GetComponent<PlayerAttack>().bulletCountAssault += (numberOfBullets * 2);
-                    collectText.SetText("");
-                    Destroy(this);
-                }
-            }
-            
-        }
-        else{
-            collectText.SetText("");
         }
     }
 }
