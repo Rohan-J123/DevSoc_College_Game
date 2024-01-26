3c7ab79eb74fc7f76bc0683d5004beb736146725
Rohan-J123 <rohanjha@aol.in>
2024-01-26 22:49:23 +0530

    Added explosion sound to explosions

--- DevSoc_College_Game/Assets/Scripts/GrenadeBehaviour.cs
+++ DevSoc_College_Game/Assets/Scripts/GrenadeBehaviour.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class GrenadeBehaviour : MonoBehaviour
 {
     [SerializeField] private GameObject explosionFire;
+
     // Start is called before the first frame update
     void Start()
     {
